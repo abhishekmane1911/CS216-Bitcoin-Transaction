b55a8cf26fa8e30c6eb5a3c4dba80a21520d7292
Vinod <vinodekdhoke@gmail.com>
2026-03-10 11:10:40 +0530

    Initial commit for CS216 Assignment

---
+++ Part2_SegWit/part2.py
@@ -0,0 +1,9 @@
+# Placeholder for Part 2 P2SH-SegWit transactions
+# A' -> B' -> C'
+
+# 1. Connect to bitcoind
+# 2. Create wallet, generate A', B', C'
+# 3. Fund A'
+# 4. A' -> B' transaction
+# 5. B' -> C' transaction
+# 6. Decode and analyze scripts
